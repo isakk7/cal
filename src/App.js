d25fbd1b2f62d3597209fc003a261f31bb49b6d3
Isaac Lopez <isaaclopez@Isaacs-MacBook-Air.local>
2022-06-18 20:06:52 -0500

    feat: finish adding all buttoms, style: style is half done

--- src/App.js
+++ src/App.js
@@ -1,36 +1,78 @@
 import { useState } from 'react';
 
 function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const ops = ["+", "-", "*", "/", "."];
+  const updateCalc = value  => {
+    if (
+      // eslint-disable-next-line
+        ops.includes(value) && calc === '' ||
+        // eslint-disable-next-line
+        ops.includes(value) && ops.includes(calc.slice(-1))
+    )
+    {
+        return;
+    }
+    setCalc(calc + value);
+// eslint-disable-next-line
+    if (!ops.includes(value)) {
+      // eslint-disable-next-line
+        setResult(eval(calc + value).toString());
+    }
+
+
+  } 
+
 
   const crearDigitos = () => {
     const digitos = [];
     for (let i = 0; i < 10; i++) {
-      digitos.push( <button key={i}>{i}</button>)
+      digitos.push( <button onClick={() => updateCalc(i.toString())} key={i}>{i}</button>)
     }
+
+    return digitos;
+  }
+// eslint-disable-next-line
+  const calcular = () => {
+    // eslint-disable-next-line
+    setCalc(eval(calc).toString());
+  }
+
+  const limpiar = () => {
+    if (calc === '') {
+      return;
+    }
+
+    const value = calc.slice(0, -1);
+    setCalc(value);
+  }
+
 
   return (
     <div className="App">
        <div className="calculadora">
        <div className="display">
-         <span>(0)</span> 0
+        {result ? <span>({result})</span> : '' }&nbsp; {calc || "0" } 
          </div>
 
          <div className="operators">
-           <button>/</button>
-           <button>+</button>
-           <button>-</button>
-           <button>*</button>
-           <button>DEL</button>
+           <button onClick={() => updateCalc('/')}>/</button>
+           <button onClick={() => updateCalc('+')}>+</button>
+           <button onClick={() => updateCalc('-')}>-</button>
+           <button onClick={() => updateCalc('*')}>*</button>
+           <button onClick={limpiar}>DEL</button>
          </div>
          <div className="digitos">
            {crearDigitos()}
-           <button>0</button>
-           <button>.</button>
-           <button>=</button>
+           <button onClick={() => updateCalc('0')}>0</button>
+           <button onClick={() => updateCalc('.')}>.</button>
+           <button onClick={calcular}>=</button>
          </div>
        </div>
     </div>
   );
 }
 
-}
+export default App;
